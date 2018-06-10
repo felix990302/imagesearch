79b0683898204be820d41d1152099b9e0c95271f
Felix Zhou <felix990302@yahoo.ca>
2018-06-09 22:01:41 -0400

    updated __init__.py with absolute relative pathing so methods can be imported correctly

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from imagesearch import region_grabber, imagesearcharea, click_image, imagesearch, imagesearch_loop, imagesearch_region_loop
+from .imagesearch import region_grabber, imagesearcharea, click_image, imagesearch, imagesearch_loop, imagesearch_region_loop
